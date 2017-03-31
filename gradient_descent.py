cd19cd5b556f66f3008d61a3d61865ec42d2f028
lach-killerpythons <lachlanmcbride5@gmail.com>
2017-03-31 17:57:18 +1100

    new notebook with math

--- gradient_descent.py
+++ gradient_descent.py
@@ -43,6 +43,29 @@ def gradient_descent_runner(points, initial_b, initial_m, learning_rate, num_ite
         b, m = step_gradient(b, m, array(points), learning_rate)
     return [b,m]
 
+
+def SGD(b_current, m_current, points, learning_rate):
+    b_gradient = 0
+    m_gradient = 0
+    N = float(len(points))
+    for i in range(0,len(points)):
+        x = points[i, 0]
+        y = points[i, 1]
+        b_gradient += -(2/N) * (y - ((m_current * x) + b_current))
+        m_gradient += -(2/N) * x * (y - ((m_current * x) + b_current))
+    new_b = b_current - (learning_rate * b_gradient)
+    new_m = m_current - (learning_rate * m_gradient)
+    return [new_b, new_m]
+
+def SGD_run(points, initial_b, initial_m, learning_rate, num_iterations):
+    b = initial_b
+    m = initial_m
+    for i in range(num_iterations):
+        b, m = step_gradient(b, m, array(points), learning_rate)
+    return [b,m]
+
+
+
 def run():
     points = genfromtxt('data.csv', delimiter=",")
     [b,m] = gradient_descent_runner(points, initial_b, initial_m, learning_rate, num_iterations)
